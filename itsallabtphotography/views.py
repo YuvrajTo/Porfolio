3a9e1e1367293ae94aab3b8dea1be18d41c9b61a
Yuvraj Todankar <yuvrajtodankar123@gmail.com>
2020-09-13 16:11:51 +0530

    Commit 6

--- itsallabtphotography/views.py
+++ itsallabtphotography/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def index(request):
+    return HttpResponse("Index")
+
 def stories(request):
     return HttpResponse("Stories")
 
